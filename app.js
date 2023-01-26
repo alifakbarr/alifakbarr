7557865614d2a0c0378c2b6194bd70f9d69efd24
Alif Akbar Irdhobilla <alifakbar8800@gmail.com>
2023-01-26 19:29:52 +0700

    add: profile in index page

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ var app = express();
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
+app.use('/assets',express.static('public'))
 app.use(methodOverride("_method"))
 
 app.use(logger('dev'));
