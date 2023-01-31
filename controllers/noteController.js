b2fa754d6ac200d9611bab2c0fadc4b3adc81e62
Alif Akbar Irdhobilla <alifakbar8800@gmail.com>
2023-01-31 20:56:27 +0700

    add: detail note

--- controllers/noteController.js
+++ controllers/noteController.js
@@ -3,12 +3,12 @@ const Note = require("../models/Note")
 
 module.exports = {
     // View Note
-    viewNote:async (req,res) => {
+    viewNote:async (req, res) => {
         try {
             const notes = await Note.find()
 
         res.render("./notes/index", {
-            notes,
+            notes
         })
         } catch (error) {
             res.redirect("/notes")
@@ -32,6 +32,18 @@ module.exports = {
             res.redirect("/notes")
         }
     },
+    detailNote: async(req,res)=>{
+        try{
+            let id = req.params.id
+            const note = await Note.findOne({_id:id})
+
+            res.render("./notes/detail",{
+                note
+            })
+        } catch (error){
+            res.redirect("/notes")
+        }
+    },
     editNote: async(req,res)=>{
         try{
             let id = req.params.id
@@ -46,10 +58,12 @@ module.exports = {
     },
     updateNote: async(req,res)=>{
         try{
+            updated_at = Date.now()
             await Note.findByIdAndUpdate(req.params.id,{
                 $set:{
                     title:req.body.title,
-                    content:req.body.content
+                    content:req.body.content,
+                    updated_at: updated_at
                 }
             }) 
             res.redirect("/notes")
